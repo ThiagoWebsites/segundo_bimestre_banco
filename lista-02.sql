8c4c3886c49f4704cf532af82320f1c2e182af16
ThiagoWebsites <143205226+ThiagoWebsites@users.noreply.github.com>
2024-05-28 17:34:11 -0300

    Término da lista 2

--- lista-02.sql
+++ lista-02.sql
@@ -12,6 +12,26 @@ SELECT *
 FROM Livros
 where ano_publicacao between 2000 and 2010;
 
+SELECT autor, count(titulo)
+FROM Livros
+group by autor
+having count(titulo) > 1;
+
+SELECT *
+from Livros
+where titulo like '%Senhor';
+
+SELECT *
+from Livros
+where categoria in ("Fantasia", "Ficção Científica", "Fábula");
+
+SELECT DISTINCT idioma
+FROM Livros;
+
 SELECT *
 FROM Livros
-where titulo like "%Senhor%";
+where quantidade_paginas between 200 and 400;
+
+SELECT titulo, ano_publicacao
+FROM Livros
+order by ano_publicacao asc;
